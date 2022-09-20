1b648c85c4a0d91bb4703db47b6ef23562f34ecd
isocodes51 <flyingcow51@hotmail.com>
2022-09-20 23:43:38 +0300

    20.09.2022 23:42 Add methodu ile yeni bir Cars vt'ye yeni bir Araba eklendi.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -10,9 +11,18 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            foreach (var _car in carManager.GetCarsByBrandId(1))
+            //foreach (var _car in carManager.GetCarsByBrandId(1))
+            //{
+            //    Console.WriteLine(_car.BrandId);
+            //}
+
+            carManager.Add(new Car {
+                BrandId=1, ColorId=1, Name="Mercedes", ModelYear="2016", DailyPrice=10000, Description="Mercedes C200 GLA"  
+                });
+
+            foreach (var car in carManager.GetAll())
             {
-                Console.WriteLine(_car.BrandId);
+                Console.WriteLine(car.Name);
             }
         }
     }
